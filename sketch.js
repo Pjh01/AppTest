e867c5f2db9c64f6cfc6f7740751d7fb33eb6a7e
Pjh01 <estelle0102@naver.com>
2022-12-02 15:45:26 +0900

    chage(1202_1)

--- sketch.js
+++ sketch.js
@@ -1,31 +1,52 @@
-var wave;
-var button;
-var playing = false;
+// var wave;
+// var button;
+// var playing = false;
 
-function setup() {
-  //createCanvas(displayWidth, displayHeight);
-  createCanvas(720, 256);
-  wave = new p5.Oscillator();
-  wave.setType('sine');
-  //wave.start();
-  wave.freq(440);   //Hz
-  wave.amp(0);      //볼륨
+// function setup() {
+//   //createCanvas(displayWidth, displayHeight);
+//   createCanvas(720, 256);
+//   wave = new p5.Oscillator();
+//   wave.setType('sine');
+//   //wave.start();
+//   wave.freq(440);   //Hz
+//   wave.amp(0);      //볼륨
 
-  button = createButton('play/pause');
-  button.mousePressed(toggle);
+//   button = createButton('play/pause');
+//   button.mousePressed(toggle);
+// }
+
+// function draw() {
+//   //background(220);
+// }
+
+// function toggle() {
+//   if(!playing){
+//     wave.start();
+//     wave.amp(0.5, 1); //0.5크기의 소리, 1초간 감소하며 끝
+//     playing = true;
+//   }else{
+//     wave.amp(0, 1);
+//     playing = false;
+//   }
+// }
+
+/////
+
+let r, g, b;
+function setup(){
+  createCanvas(displayWidth, displayHeight);
+  r = random(50, 255);
+  g = random(0, 200);
+  b = random(50, 255);
 }
 
 function draw() {
-  //background(220);
+  background(r, g, b);
+  console.log('draw');
 }
 
-function toggle() {
-  if(!playing){
-    wave.start();
-    wave.amp(0.5, 1);
-    playing = true;
-  }else{
-    wave.amp(0, 1);
-    playing = false;
-  }
+function deviceMoved(){
+  r = map(accelerationX, -90, 90, 100, 175);
+  g = map(accelerationY, -90, 90, 100, 200);
+  b = map(accelerationZ, -90, 90, 100, 200);
 }
